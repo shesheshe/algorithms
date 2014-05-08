0c65cc12ff85eda87745d45951a355d35524033c
hero.hsu <shesheshe98@hotmail.com>
2014-05-08 17:28:16 +0800

    ++ -> ok, -- -> ok, +- | -+ --> ...

--- javascript/big_number.js
+++ javascript/big_number.js
@@ -1,5 +1,6 @@
 /*
 * 正數 + 正數 ok
+* 負數 + 負數 ok
 * 正數 + 負數 ...
 */
 var bn = function(a, b) {
@@ -9,55 +10,51 @@ var bn = function(a, b) {
 	var aLength = a.length;
 	var bLength = b.length;
 
+	function run(tmp, i) {
+
+		if(parseInt(tmp/10) == 0) {
+			arr[i] = tmp;
+			tmp2 = 0;
+		} else {
+			arr[i] = parseInt(tmp%10);
+			tmp2 = parseInt(tmp/10);
+		}
+
+		
+	}
+	
+	a = positive2negative(a);
+	b = positive2negative(b);
+
 	a = a.reverse();
 	b = b.reverse();
 
 	for(i=0; i<max; i++) {
-	
 		if(a[i] != undefined) {
-			
 			if(b[i] != undefined) {
-				
-				tmp = a[i] + b[i] + tmp2;
-
-				if(parseInt(tmp/10) == 0) {
-					arr[i] = tmp;
-					tmp2 = 0;
-				} else {
-					arr[i] = parseInt(tmp%10);
-					tmp2 = parseInt(tmp/10);
-				}
+			
+				run(a[i] + b[i] + tmp2, i);
 				
 			} else {
-				tmp = a[i] + tmp2;
-				if(parseInt(tmp/10) == 0) {
-					arr[i] = tmp;
-					tmp2 = 0;
-				} else {
-					arr[i] = parseInt(tmp%10);
-					tmp2 = parseInt(tmp/10);
-				}
+			
+				run(a[i] + tmp2, i);
+				
 			}
 			
 		} else {
-		
 			if(b[i] != undefined) {
-
-				tmp = b[i] + tmp2;
-				if(parseInt(tmp/10) == 0) {
-					arr[i] = tmp;
-					tmp2 = 0;
-				} else {
-					arr[i] = parseInt(tmp%10);
-					tmp2 = parseInt(tmp/10);
-				}
+			
+				run(b[i] + tmp2, i);
+				
 			} else {
-				arr[i] = tmp2;
+			
+				arr[i] = tmp2==0?'-':tmp2;
 				tmp2 = '-';
+				
 			}
 		}
 	}
-	
+
 	arr.reverse();
 	
 	for(i=0; i<max; i++) {
@@ -65,13 +62,54 @@ var bn = function(a, b) {
 			arr_tmp[j++] = arr[i];
 		}
 	}
+
+	arr_tmp = negative2positive(arr_tmp);
 	
 	return arr_tmp;
-	
+
 }
 
-arr = [5, 5, 5];
-arr2 = [5, 5, 5];
+/*
+* 假如陣列第一個元素是[ - ], 就把每個元素乘以負1
+*/
+var positive2negative = function(arr) {
+	var len = arr.length;
+	var i = 0;
+
+	if(arr[0] == '-') {
+		for(i=0; i<len; i++) {
+			if(arr[i+1] != undefined) {
+				arr[i] = arr[i+1] * -1;
+			}
+		}
+		arr.pop();
+	}
+
+	return arr;
+}
+
+/*
+* 假如陣列第一個元素是負數, 就把每個元素變成絕對值, 並把第一個元素變成[ - ]
+*/
+var negative2positive = function(arr) {
+	var len = arr.length;
+	var i = 0;
+
+	if(arr[0] <= -1) {
+		arr.unshift('-');
+		len = len + 1;
+		for(i=1; i<len; i++) {
+			if(arr[i] != undefined) {
+				arr[i] = Math.abs(arr[i]);
+			}
+		}
+	}
+
+	return arr;
+}
+
+arr = ['-', 5];
+arr2 = ['-', 1, 0];
 
 _arr = bn(arr, arr2);
 console.log(arr);
